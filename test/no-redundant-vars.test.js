3925928ac17916b3e19e8c039056d77500351b1a
ej-shafran <ejshafran@hilma.tech>
2023-11-09 14:45:59 +0200

    chore(tests): restructure test inputs

--- test/no-redundant-vars.test.js
+++ test/no-redundant-vars.test.js
@@ -9,63 +9,61 @@ ruleTester.run("no-redundant-vars", rule, {
   valid: [
     {
       name: "should ignore a simple return",
-      code: `\
-function temporary() {
-  return 10;
-}`,
+      code: ["function temporary() {", "  return 10;", "}"].join("\n"),
     },
     {
       name: "should ignore a used variable",
-      code: `\
-function temporary() {
-  const x = 10;
-  console.log(x);
-  return x;
-}`,
+      code: [
+        "function temporary() {",
+        "  const x = 10;",
+        "  console.log(x);",
+        "  return x;",
+        "}",
+      ].join("\n"),
     },
     {
       name: "should ignore an empty return",
-      code: `\
-function temporary() {
-  return;
-}`,
+      code: ["function temporary() {", "  return;", "}"].join("\n"),
     },
     {
       name: "should ignore a destructured `await`",
-      code: `\
-async function temporary() {
-  const { result } = await doSomething();
-  return result;
-}
-
-async function other() {
-  const [result] = await doSomething();
-  return result;
-}`,
+      code: [
+        "async function temporary() {",
+        "  const { result } = await doSomething();",
+        "  return result;",
+        "}",
+        "",
+        "async function other() {",
+        "  const [result] = await doSomething();",
+        "  return result;",
+        "}",
+      ].join("\n"),
     },
     {
       name: "should ignore one of many destructured elements",
-      code: `\
-function temporary() {
-  const { keyA, keyB } = obj;
-  return keyB;
-}
-
-function other() {
-  const [elA, elB] = arr;
-  return elA;
-}`,
+      code: [
+        "function temporary() {",
+        "  const { keyA, keyB } = obj;",
+        "  return keyB;",
+        "}",
+        "",
+        "function other() {",
+        "  const [elA, elB] = arr;",
+        "  return elA;",
+        "}",
+      ].join("\n"),
     },
   ],
 
   invalid: [
     {
       name: "should error for a redundant variable",
-      code: `\
-function temporary() {
-  const x = 10;
-  return x;
-}`,
+      code: [
+        "function temporary() {",
+        "  const x = 10;",
+        "  return x;",
+        "}",
+      ].join("\n"),
       errors: [
         {
           messageId: "noRedundantVars",
@@ -73,11 +71,12 @@ function temporary() {
             {
               messageId: "inlineVariable",
               data: { variable: "x" },
-              output: `\
-function temporary() {
-  
-  return 10;
-}`,
+              output: [
+                "function temporary() {",
+                "  ",
+                "  return 10;",
+                "}",
+              ].join("\n"),
             },
           ],
         },
@@ -85,11 +84,12 @@ function temporary() {
     },
     {
       name: "should work for multiple declarations",
-      code: `\
-function temporary() {
-  const a = 10, b = 20, c = 30;
-  return b;
-}`,
+      code: [
+        "function temporary() {",
+        "  const a = 10, b = 20, c = 30;",
+        "  return b;",
+        "}",
+      ].join("\n"),
       errors: [
         {
           messageId: "noRedundantVars",
@@ -97,11 +97,12 @@ function temporary() {
             {
               messageId: "inlineVariable",
               data: { variable: "b" },
-              output: `\
-function temporary() {
-  const a = 10, c = 30;
-  return 20;
-}`,
+              output: [
+                "function temporary() {",
+                "  const a = 10, c = 30;",
+                "  return 20;",
+                "}",
+              ].join("\n"),
             },
           ],
         },
@@ -109,11 +110,12 @@ function temporary() {
     },
     {
       name: "should properly handle object destructuring",
-      code: `\
-function temporary() {
-  const { result } = data;
-  return result;
-}`,
+      code: [
+        "function temporary() {",
+        "  const { result } = data;",
+        "  return result;",
+        "}",
+      ].join("\n"),
       errors: [
         {
           messageId: "noRedundantVars",
@@ -121,11 +123,12 @@ function temporary() {
             {
               messageId: "inlineVariable",
               data: { variable: "result" },
-              output: `\
-function temporary() {
-  
-  return data.result;
-}`,
+              output: [
+                "function temporary() {",
+                "  ",
+                "  return data.result;",
+                "}",
+              ].join("\n"),
             },
           ],
         },
@@ -133,16 +136,17 @@ function temporary() {
     },
     {
       name: "should properly handle array destructring",
-      code: `\
-function temporary() {
-  const [result] = data;
-  return result;
-}
-
-function other() {
-  const [, result] = data;
-  return result;
-}`,
+      code: [
+        "function temporary() {",
+        "  const [result] = data;",
+        "  return result;",
+        "}",
+        "",
+        "function other() {",
+        "  const [, result] = data;",
+        "  return result;",
+        "}",
+      ].join("\n"),
       errors: [
         {
           messageId: "noRedundantVars",
@@ -150,16 +154,17 @@ function other() {
             {
               messageId: "inlineVariable",
               data: { variable: "result" },
-              output: `\
-function temporary() {
-  
-  return data[0];
-}
-
-function other() {
-  const [, result] = data;
-  return result;
-}`,
+              output: [
+                "function temporary() {",
+                "  ",
+                "  return data[0];",
+                "}",
+                "",
+                "function other() {",
+                "  const [, result] = data;",
+                "  return result;",
+                "}",
+              ].join("\n"),
             },
           ],
         },
@@ -169,16 +174,17 @@ function other() {
             {
               messageId: "inlineVariable",
               data: { variable: "result" },
-              output: `\
-function temporary() {
-  const [result] = data;
-  return result;
-}
-
-function other() {
-  
-  return data[1];
-}`,
+              output: [
+                "function temporary() {",
+                "  const [result] = data;",
+                "  return result;",
+                "}",
+                "",
+                "function other() {",
+                "  ",
+                "  return data[1];",
+                "}",
+              ].join("\n"),
             },
           ],
         },
@@ -186,11 +192,12 @@ function other() {
     },
     {
       name: "should wrap complicated expressions in parens",
-      code: `\
-function temporary() {
-  const { result } = a ? b : c;
-  return result;
-}`,
+      code: [
+        "function temporary() {",
+        "  const { result } = a ? b : c;",
+        "  return result;",
+        "}",
+      ].join("\n"),
       errors: [
         {
           messageId: "noRedundantVars",
@@ -198,11 +205,12 @@ function temporary() {
             {
               messageId: "inlineVariable",
               data: { variable: "result" },
-              output: `\
-function temporary() {
-  
-  return (a ? b : c).result;
-}`,
+              output: [
+                "function temporary() {",
+                "  ",
+                "  return (a ? b : c).result;",
+                "}",
+              ].join("\n"),
             },
           ],
         },
