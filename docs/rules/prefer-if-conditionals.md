a8d9b3978064268e22d9fd172ddd29e01fba230f
ej-shafran <ejshafran@hilma.tech>
2024-01-11 12:10:39 +0200

    feat(rule): add `prefer-if-conditionals`

---
+++ docs/rules/prefer-if-conditionals.md
@@ -0,0 +1,25 @@
+# Prefer an `if` statement to using logical operators (`&&` or `||`) when relevant (`nitpick/prefer-if-conditionals`)
+
+💼 This rule is enabled in the following configs: 🌐 `all`, ✅ `recommended`.
+
+🔧💡 This rule is automatically fixable by the [`--fix` CLI option](https://eslint.org/docs/latest/user-guide/command-line-interface#--fix) and manually fixable by [editor suggestions](https://eslint.org/docs/developer-guide/working-with-rules#providing-suggestions).
+
+<!-- end auto-generated rule header -->
+
+## Rule Details
+
+❌ **Incorrect** code:
+
+```js
+isOk && doSomething();
+(!isBad && isGood) || logError();
+myObj && myObj.promise && (await myObj.promise);
+```
+
+✅ **Corrrect** code:
+
+```js
+if (isOk) doSomething();
+if (!(!isBad && isGood)) logError();
+if (myObj && myObj.promise) await myObj.promise;
+```
---
+++ test/prefer-if-conditionals.test.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const rule = require("../lib/rules/prefer-if-conditionals");
+const RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("prefer-if-conditionals", rule, {
+  valid: [
+    {
+      name: "should allow conditionals stored in variables",
+      code: [
+        "const x = isOk && someThing();",
+        "const y = isNotOk || someThing();",
+      ].join("\n"),
+    },
+    {
+      name: "should allow conditionals passed to functions",
+      code: ["someThing(a && b())", "someThing(c || d())"].join("\n"),
+    },
+  ],
+  invalid: [
+    {
+      name: "should fail for unused conditionals",
+      code: "isOk && someThing",
+      errors: [
+        {
+          messageId: "preferIfConditionals",
+          suggestions: [
+            {
+              messageId: "useIfStatement",
+              output: "if (isOk) someThing",
+            },
+          ],
+        },
+      ],
+      output: "if (isOk) someThing",
+    },
+    {
+      name: "should properly fix complex inputs",
+      code: "!isBad || isOk && someThing",
+      errors: [
+        {
+          messageId: "preferIfConditionals",
+          suggestions: [
+            {
+              messageId: "useIfStatement",
+              output: "if (!isBad || isOk) someThing",
+            },
+          ],
+        },
+      ],
+      output: "if (!isBad || isOk) someThing",
+    },
+    {
+      name: "should properly invert `||` cases",
+      code: "!isBad || isOk || someThing",
+      errors: [
+        {
+          messageId: "preferIfConditionals",
+          suggestions: [
+            {
+              messageId: "useIfStatement",
+              output: "if (!(!isBad || isOk)) someThing",
+            },
+          ],
+        },
+      ],
+      output: "if (!(!isBad || isOk)) someThing",
+    },
+  ],
+});
