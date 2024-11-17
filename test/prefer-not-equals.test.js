99568c792a52056d1d88e400f4beeb10f43e869f
ej shafran <116496520+ej-shafran@users.noreply.github.com>
2024-11-17 12:13:17 +0200

    feat(rules): add prefer-not-equals (#54)
    * feat(rules): add placeholder rule
    
    * test: add a few basic (failing) tests
    
    * test: fix tests
    
    * fix(rules): make rule pass tests
    
    * docs: complete todos
    
    * chore: ignore files for prettier
    
    * feat: actually add rule
    
    Along with documentation
    
    * test: new tests
    
    Also, fix false positives
    
    * docs: add documentation

---
+++ test/prefer-not-equals.test.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const rule = require("../lib/rules/prefer-not-equals");
+const RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester();
+
+/**
+ * @param {"==" | "==="} operator
+ * @param {"!=" | "!=="} reverseOperator
+ * @returns {RuleTester.InvalidTestCase[]}
+ **/
+function invalidTestCases(operator, reverseOperator) {
+  return [
+    {
+      name: "should fail when negating an equality check",
+      code: `if (!(a ${operator} b)) {}`,
+      errors: [
+        {
+          messageId: "preferNotEquals",
+          suggestions: [
+            {
+              messageId: "useNotEquals",
+              output: `if ((a ${reverseOperator} b)) {}`,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "should fail even for multiple expressions",
+      code: `if (!(a ${operator} b || c.something())) {}`,
+      errors: [
+        {
+          messageId: "preferNotEquals",
+          suggestions: [
+            {
+              messageId: "useNotEquals",
+              output: `if ((a ${reverseOperator} b || !(c.something()))) {}`,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "should fail even for multiple expressions",
+      code: `if (!(c || a ${operator} b || d.something())) {}`,
+      errors: [
+        {
+          messageId: "preferNotEquals",
+          suggestions: [
+            {
+              messageId: "useNotEquals",
+              output: `if ((!(c) || a ${reverseOperator} b || !(d.something()))) {}`,
+            },
+          ],
+        },
+      ],
+    },
+  ];
+}
+
+ruleTester.run("prefer-not-equals", rule, {
+  valid: [
+    {
+      name: "should allow comparing things with !==",
+      code: ["if (a !== b) {}"].join("\n"),
+    },
+    {
+      name: "should allow comparing things with !=",
+      code: ["if (a != b) {}"].join("\n"),
+    },
+    {
+      name: "should allow inverting other operators",
+      code: ["if (!a()) {}"].join("\n"),
+    },
+  ],
+  invalid: [...invalidTestCases("===", "!=="), ...invalidTestCases("==", "!=")],
+});
